77ef977ab12a8490d854a19b1ecd178633beecc0
Tomás Lautaro Euliarte <tomaslautaroeuliarte@gmail.com>
2023-03-23 14:57:56 -0300

    añadido archivo de propiedades para deploy y propiedades de base de datos

--- src/main/java/com/myblog/tomaseuliarte/TomaseuliarteApplication.java
+++ src/main/java/com/myblog/tomaseuliarte/TomaseuliarteApplication.java
@@ -1,12 +1,13 @@
 package com.myblog.tomaseuliarte;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TomaseuliarteApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
+           
+
 		SpringApplication.run(TomaseuliarteApplication.class, args);
 	}
 
