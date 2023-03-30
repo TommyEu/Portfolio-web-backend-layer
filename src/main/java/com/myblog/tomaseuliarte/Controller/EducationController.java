29a758a1fc973377125fb55b26ba362647623f48
Tomás Lautaro Euliarte <tomaslautaroeuliarte@gmail.com>
2023-03-30 12:54:57 -0300

    arreglado bug en metodo delete

--- src/main/java/com/myblog/tomaseuliarte/Controller/EducationController.java
+++ src/main/java/com/myblog/tomaseuliarte/Controller/EducationController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,7 +41,7 @@ public class EducationController {
         eduServ.updateEducation(edu);
     }   
     @DeleteMapping("/delete/{id}")
-    public void borrarEducation(Long id){
+    public void borrarEducation(@PathVariable Long id){
         eduServ.deleteInfo(id);
     }
 }
